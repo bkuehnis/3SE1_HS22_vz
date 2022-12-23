024b86f82d3331388b9583b2fe2b2d61a89b4ac7
bkuehnis <62989491+bkuehnis@users.noreply.github.com>
2022-12-23 10:49:40 +0100

    fixed error

--- src/main/java/uebung13/vza/ch/zhaw/drmicfil/JokesOfDay.java
+++ src/main/java/uebung13/vza/ch/zhaw/drmicfil/JokesOfDay.java
@@ -10,8 +10,6 @@ import javax.swing.JOptionPane;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
-import uebung13.Joke;
-
 public class JokesOfDay {
 
 	public static void main(String[] args) {
